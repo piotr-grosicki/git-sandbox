bf48f8218eea470c60590beef153b4f5e0c89fd5
Piotr Grosicki <piotr.grosicki@collibra.com>
2020-10-27 15:11:29 +0100

    test5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,25 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("ELO");
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
 	}
 
 }
