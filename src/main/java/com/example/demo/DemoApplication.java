fdb8336c6ae7aae064f986e0c223c0c26043958b
Piotr Grosicki <piotr.grosicki@collibra.com>
2020-10-27 15:10:12 +0100

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
+@RequestMapping
 public class DemoApplication {
+
+	private String elo = "elo";
+
 
 	public static void main(String[] args) {
 		System.out.println("ELO");
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		System.out.println("ELO");
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("ELO");
+		System.out.println("ELO");
 	}
 
 }
